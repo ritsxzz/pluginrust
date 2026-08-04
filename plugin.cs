94dfc5a6e31d2e4fcc23e3bd7ba373ad063b0f37
ritsxzz <minorilock69@gmail.com>
2026-08-04 03:47:45 +0300

    Add ServerConnectorPlugin for server connection

---
+++ plugin.cs
@@ -0,0 +1,27 @@
+using BepInEx;
+using UnityEngine;
+
+namespace MyCustomPlugin
+{
+    [BepInPlugin("com.myproject.serverconnector", "Server Connector", "1.0.0")]
+    public class ServerConnectorPlugin : BaseUnityPlugin
+    {
+        private const string ServerIP = "217.60.245.104";
+        private const int ServerPort = 20000;
+
+        void OnGUI()
+        {
+            // Отрисовка простой кнопки прямо в меню игры
+            if (GUI.Button(new Rect(10, 10, 200, 40), "Подключиться к серверу"))
+            {
+                ConnectToServer();
+            }
+        }
+
+        private void ConnectToServer()
+        {
+            // Вызов встроенной консольной команды Rust для подключения
+            ConsoleSystem.Run(ConsoleSystem.Option.Client, $"connect {ServerIP}:{ServerPort}");
+        }
+    }
+}
